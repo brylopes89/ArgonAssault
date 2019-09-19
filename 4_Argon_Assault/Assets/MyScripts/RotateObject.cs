ba78cce7ce03b354316cf0bfd404cc3ce4baaf6e
Bryan Lopes <brylopes89@gmail.com>
2019-09-19 16:57:39 -0400

    Player Ship added. Skybox changed.

---
+++ 4_Argon_Assault/Assets/MyScripts/RotateObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour
+{
+    public float Speed = 50.0f;
+    //public GameObject Planetoid;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up * Speed * Time.deltaTime);
+        //transform.RotateAround(Planetoid.transform.position, Vector3.up, Speed * Time.deltaTime);
+    }
+}
