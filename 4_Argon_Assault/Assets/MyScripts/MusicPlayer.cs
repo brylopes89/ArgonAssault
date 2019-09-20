fde6cb71668417f1f4be3ee142f39d88f3ee2749
Bryan Lopes <brylopes89@gmail.com>
2019-09-20 17:34:02 -0400

    Added Standard assets package. Created circuit waypoints that camera follows

---
+++ 4_Argon_Assault/Assets/MyScripts/MusicPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+    public float ChangeTime = 6f;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        Invoke("LoadFirstScene", ChangeTime);
+    }
+
+    void LoadFirstScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
