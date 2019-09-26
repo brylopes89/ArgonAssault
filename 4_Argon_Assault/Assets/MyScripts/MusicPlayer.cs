c164dedc2553616dfa70ea52927f2c78d86ba272
Bryan Lopes <brylopes89@gmail.com>
2019-09-26 18:48:58 -0400

    Singleton pattern without statics

--- 4_Argon_Assault/Assets/MyScripts/MusicPlayer.cs
+++ 4_Argon_Assault/Assets/MyScripts/MusicPlayer.cs
@@ -7,7 +7,17 @@ public class MusicPlayer : MonoBehaviour
     // Start is called before the first frame update
     private void Awake()
     {
-        DontDestroyOnLoad(gameObject);
+        // if more than one music player in scene, destroy ourselves
+        int numMusicPlayers = FindObjectsOfType<MusicPlayer>().Length;
+
+        if (numMusicPlayers > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }        
     }
 
 }
