2bb1a10195006a4df987a193b37d3259ced66a31
Bryan Lopes <brylopes89@gmail.com>
2019-09-26 18:30:51 -0400

    Implemented reload after ship collision and explosion trigger.
    Collision handler adjusted to invoke a reloadscene scene and levelload delay. Also Adjusted Splash scene core ships scaling and brake speed.

--- 4_Argon_Assault/Assets/MyScripts/CollisionHandler.cs
+++ 4_Argon_Assault/Assets/MyScripts/CollisionHandler.cs
@@ -2,18 +2,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+    [Tooltip("In seconds")][SerializeField] float levelLoadDelay = 2f;
+    [Tooltip("FX pefab on player")][SerializeField] GameObject deathFX;
+
+
     private void OnTriggerEnter(Collider other)
     {
-
-        StartDeathSequence();            
-        
+        StartDeathSequence();
+        deathFX.SetActive(true);
+        Invoke("ReloadScene", levelLoadDelay);
     }
     void StartDeathSequence()
     {
         print("Wipe yourself off, you dead.");
         SendMessage("OnPlayerDeath");
+    }
+
+    private void ReloadScene() // string referenced
+    {
+        SceneManager.LoadScene(1);
     }
 }
