c06fd0160f947e8f6786db4d759b01429c83168e
Bryan Lopes <brylopes89@gmail.com>
2019-09-27 12:22:07 -0400

    Particle detection script added.
    Enemy is destroyed on collision. Enemy script also generates its own box collider component, rather than rely on adjusting the asset pack prefabs.

---
+++ 4_Argon_Assault/Assets/MyScripts/Enemy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        AddNonTriggerBoxCollider();
+    }
+
+    private void AddNonTriggerBoxCollider()
+    {
+        Collider boxCollider = gameObject.AddComponent<BoxCollider>();
+        boxCollider.isTrigger = false;
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        Destroy(gameObject);
+    }
+
+}
