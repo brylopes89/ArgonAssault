de2955f942c45c0a91b6b10f5b370c8b7f06ab0c
Bryan Lopes <brylopes89@gmail.com>
2019-10-02 19:02:36 -0400

    Added master timeline track

---
+++ 4_Argon_Assault/Assets/MyScripts/Test.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Test : MonoBehaviour
+{   
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        int num1, num2, num3, result;
+
+        Console.WriteLine("Input the first number to multiply: ");
+        num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Input the second number to multiply: ");
+        num2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Input the third number to multiply: ");
+        num3 = Convert.ToInt32(Console.ReadLine());
+
+        result = num1 * num2 * num3;
+
+        Console.WriteLine("{0}x{1}x{2} = {3}", num1, num2, num3, result);
+
+        Console.ReadKey();
+    } 
+}
