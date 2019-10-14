146fc15fb9002cb23886e40d1449d29540e8c249
Bryan Lopes <brylopes89@gmail.com>
2019-10-14 19:14:41 -0400

    Revert "Added Flight parent"
    This reverts commit a2584e575b1b17654a69aea0f9cacc44dc0103ed.

--- 4_Argon_Assault/Assets/MyScripts/ParticleEmission.cs
+++ 4_Argon_Assault/Assets/MyScripts/ParticleEmission.cs
@@ -8,10 +8,13 @@ using static UnityEngine.ParticleSystem;
 public class ParticleEmission : MonoBehaviour
 {
     public GameObject[] thrusters; //thruster emission   
+   
 
-    List<ParticleSystem> particleSystems = new List<ParticleSystem>();
+
+    ParticleSystem ps;
     ParticleSystem.MinMaxCurve[] em;
-    
+    ParticleSystem.MainModule main;
+
 
     float min = 0.2f;
     float max = 5.0f;
@@ -19,37 +22,35 @@ public class ParticleEmission : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        ps = gameObject.GetComponentInChildren<ParticleSystem>();
+        //ps = GetComponentsInChildren<ParticleSystem>()[thrusters.Length];
+       // ps = GetComponents<GameObject>ParticleSystem
+        main = ps.main;
 
-        foreach(GameObject obj in thrusters)
-        {
-            var ps = obj.GetComponent<ParticleSystem>();
-            particleSystems.Add(ps);     
-        }
     }
+
+   
 
     // Update is called once per frame
     void Update()
     {
+
+        main.startSpeed = new ParticleSystem.MinMaxCurve(min, max);
+        main.simulationSpeed = 1;
         CurveIncrease();
+      
     }
 
     void CurveIncrease()
     {
-        foreach (ParticleSystem ps in particleSystems) // 
+        if (Input.GetAxis("Thrust") > 0)
         {
-            var main = ps.main;            
-
-            if (Input.GetAxis("Thrust") > 0)
-            {               
+            foreach (GameObject thruster in thrusters)
+            {
                 main.startSpeed = new ParticleSystem.MinMaxCurve(50.0f, 100.0f);
                 main.simulationSpeed = 100;
             }
-            else
-            {
-                main.startSpeed = new ParticleSystem.MinMaxCurve(min, max);
-                main.simulationSpeed = 1;
-            }
-        }      
-              
+        }
+        
     } 
 }
